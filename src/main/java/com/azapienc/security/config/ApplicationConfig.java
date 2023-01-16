357dc4dcf7198a937b0e96597d6d4b599eebb12d
Armando Zapien <azapienc@0715AZAPIEN.netlogistik.local>
2023-01-16 15:27:44 -0600

    Security implemented

--- src/main/java/com/azapienc/security/config/ApplicationConfig.java
+++ src/main/java/com/azapienc/security/config/ApplicationConfig.java
@@ -2,9 +2,14 @@ package com.azapienc.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.azapienc.security.user.UserRepository;
 
@@ -21,5 +26,24 @@ public class ApplicationConfig {
     return username -> userRepository
         .findByEmail(username)
         .orElseThrow(() -> new UsernameNotFoundException("User not found..."));
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
+
+    return authProvider;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    return config.getAuthenticationManager();
   }
 }
