280a4e7a19e619d2e6c5cd65e01f4854ba3c50ec
azapienc <azapienc@gmail.com>
2023-01-17 14:38:49 -0600

    log file added

--- src/main/java/com/azapienc/security/demo/UserController.java
+++ src/main/java/com/azapienc/security/demo/UserController.java
@@ -11,15 +11,18 @@ import com.azapienc.security.service.UserBasicResponse;
 import com.azapienc.security.service.UserService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
+@Slf4j
 public class UserController {
   private final UserService userService;
 
   @GetMapping("/all-users")
   public ResponseEntity<List<UserBasicResponse>> getAll() {
+    log.info("Getting all users");
     return ResponseEntity.ok(userService.getAll());
   }
 }
